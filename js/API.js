ce2701286ee08970e18d8411fe5b5ddbf0efe577
jesicaguido <jesica.guido@istea.com.ar>
2024-08-23 20:08:11 -0300

    Add files via upload

--- js/API.js
+++ js/API.js
@@ -1,23 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    fetch('https://fakestoreapi.com/products/')
-    .then(res=>res.json())
-    .then(products=> {
-        const productList=document.getElementById('product-list');
-        products.forEach(product => {
-            const productCard = `
-                    <article class="col">
-                        <figure class="card h-100">
-                            <img src="${product.image}" class="card-img-top" alt="${product.title}" width="150px">
-                            <figcaption class="card-body">
-                                <h5 class="card-title">${product.title}</h5>
-                                <p class="card-text">$${product.price}</p>
-                            </figcaption>
-                        </figure>
-                    </article>
-                `;
-            productList.insertAdjacentHTML('beforeend', productCard);                    
-        });
-    });
+    
 })
 
 
@@ -36,6 +18,24 @@ function renderProducts(products) {
                 </figure>
             </article>
         `;
-        productList.querySelector('.row').insertAdjacentHTML('beforeend', productCard);
+        productList.insertAdjacentHTML('beforeend', productCard);
     });
 }
+
+ // Función para inicializar la aplicación
+ function init() {
+    fetch('https://fakestoreapi.com/products/')
+        .then(res => res.json())
+        .then(products => {
+            renderProducts(products);
+        });
+}
+
+ // Ejecutar cuando el DOM esté completamente cargado
+ document.addEventListener('DOMContentLoaded', init);
+
+ // Función para abrir el modal con la información del producto
+function openModal(title, image, price, description) {
+    console.log("hola");
+}
+
