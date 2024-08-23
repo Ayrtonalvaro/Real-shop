c273c34ec41d813a4711af0ba1cc4af5f6931eef
jesicaguido <jesica.guido@istea.com.ar>
2024-08-23 19:48:56 -0300

    Add files via upload

--- js/API.js
+++ js/API.js
@@ -5,17 +5,37 @@ document.addEventListener('DOMContentLoaded', function () {
         const productList=document.getElementById('product-list');
         products.forEach(product => {
             const productCard = `
-                    <div class="col">
-                        <div class="card h-100">
+                    <article class="col">
+                        <figure class="card h-100">
                             <img src="${product.image}" class="card-img-top" alt="${product.title}" width="150px">
-                            <div class="card-body">
+                            <figcaption class="card-body">
                                 <h5 class="card-title">${product.title}</h5>
                                 <p class="card-text">$${product.price}</p>
-                            </div>
-                        </div>
-                    </div>
+                            </figcaption>
+                        </figure>
+                    </article>
                 `;
             productList.insertAdjacentHTML('beforeend', productCard);                    
         });
     });
 })
+
+
+// Función para renderizar productos
+function renderProducts(products) {
+    const productList = document.getElementById('product-list');
+    products.forEach(product => {
+        const productCard = `
+            <article class="col" onclick="openModal('${product.title}', '${product.image}', '${product.price}', '${product.description}')">
+                <figure class="card h-100">
+                    <img src="${product.image}" class="card-img-top" alt="${product.title}" width="150px">
+                    <figcaption class="card-body">
+                        <h5 class="card-title">${product.title}</h5>
+                        <p class="card-text">$${product.price}</p>
+                    </figcaption>
+                </figure>
+            </article>
+        `;
+        productList.querySelector('.row').insertAdjacentHTML('beforeend', productCard);
+    });
+}
